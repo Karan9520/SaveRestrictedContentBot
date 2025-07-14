a85152476c081fdef3105816c48c9f9b1e7fe7a5
Karan singh <68398500+Karan9520@users.noreply.github.com>
2025-07-14 23:02:14 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,7 @@ API_HASH = config("3ef0c04398ee56082f090ff1ff058da0", default=None)
 BOT_TOKEN = config("7616549969:AAHYTw0TeUB-x5Lm1AAPW3Y7dekiolRo2l8", default=None)
 SESSION = config("BQFH-UsAm5RjHyKlIEy4gRTzckevax5TloVdAzRS7x3K1VGc587dxqCdIPbo7HSJYvyhmLptgDWMjJERcQo874stJGVTsOEhucfH0AM7lr1MLanLy5-wh0HfnIOUOUFDYMB9eXfra4LaM9pcv3qulEa3LxOvZSKjmw3sqM0SmPqNjlMJNmK1nCl9rQZeaaxYKrJERBjdyZGctq2SHgcLDa7uoRe4BUg_fuoI5SqLHNlO5Yb0yPvJOFol_Wq4PjGeJ8FSn-gmNTGDaMdRuKO_5_jmIDTA2AkXHbCYpWBKrcvCiNjlwmb_ADc__CQc2-pvLa2itshhWUgGi6vKFNxcNJllD5xnowAAAAG8itH1AA", default=None)
 FORCESUB = config("@AwaraSupportTeam", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+AUTH = config("7165581725", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
